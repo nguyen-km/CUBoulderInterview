b08b554c8fb9e3372cc1fd81dbc9058531039c93
Kevin Nguyen <kevnguyen@Kevins-MacBook-Pro.local>
2023-05-05 21:45:43 -0600

    First commit, all files

---
+++ README.md
@@ -0,0 +1,30 @@
+# Technical Exercise
+
+- University of Colorado Boulder
+- Office of Data Analytics
+- Data Modeler and Quality Analyst
+
+## Overview
+
+The purpose of this exercise is to demonstrate your approach to reading and enhancing SQL queries. Reviewers are not looking for one specific solution but rather a conversation starter for how you approach problems.
+
+## Prompt
+
+Please rewrite/refactor this query with the goals of:
+
+- Improved readability and understanding
+- Optimized performance
+- Better simplicity
+- If applicable, fixing mistakes 
+
+## Submission
+
+When complete, please do the following:
+
+1. Commit your SQL file (named something like `my_query.sql`) to a publicly-available Github repository.
+2. Ensure the Github repository is publicly available for the duration of the hiring process.
+3. Share the link to the repository with your recruiter or interview scheduler.
+
+## Support
+
+If you have any questions about this exercise, please contact your recruiter or interview scheduler.
---
+++ my_query.sql
@@ -0,0 +1,56 @@
+SELECT --Don't need DISTINCT clause 
+    dp.dir_uid AS username,
+    de.mail AS email,
+    CASE
+        WHEN dp.primaryaffiliation = 'Staff'
+            OR daf.description NOT IN 
+                (
+                    'Student Employee',
+                    'Student Faculty'
+                )
+            OR dp.primaryaffiliation = 'Officer/Professional' 
+            OR daf.description = 'Faculty' THEN 'Faculty/Staff'
+        ELSE 'Student'
+    END AS person_type
+FROM dirsvcs.dir_person dp 
+INNER JOIN dirsvcs.dir_affiliation daf 
+    ON daf.uuid = dp.uuid 
+    AND daf.campus = 'Boulder Campus' -- Since inner join, WHERE and AND are the same
+    AND dp.primaryaffiliation NOT IN 
+        (
+            'Not currently affiliated',
+            'Retiree',
+            'Affiliate',
+            'Member'
+        ) 
+    AND daf.description NOT IN 
+        (
+            'Admitted Student',
+            'Alum',
+            'Confirmed Student',
+            'Former Student',
+            'Member Spouse',
+            'Sponsored',
+            'Sponsored EFL',
+            'Retiree',
+            'Boulder3'
+        ) 
+    AND daf.description NOT LIKE 'POI_%'
+LEFT JOIN dirsvcs.dir_email de
+    ON de.uuid = dp.uuid
+    WHERE de.mail_flag = 'M' --Changed to a where because of left join
+        AND de.mail IS NOT NULL  -- Only need if NULLs present in original de.mail variable
+WHERE dp.primaryaffiliation != 'Student'
+    OR 
+        (
+            dp.primaryaffiliation = 'Student'
+            AND EXISTS 
+                (
+                    SELECT 'x' 
+                    FROM dirsvcs.dir_acad_career 
+                    WHERE uuid = dp.uuid
+                )
+        )
+    AND lower(de.mail) NOT LIKE '%cu.edu' --removed from above beacuse of redundancy
+
+
